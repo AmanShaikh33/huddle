3d789fae9b6aa2c3526dd93e8183c33e6fab82e4
Aman Shaikh <amanshaikh7238@gmail.com>
2025-06-17 12:04:24 +0530

    Fix/update: message controller logic

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,8 +14,8 @@ const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
 router.get("/:id", isAuth, userProfile);
-router.post("/:id", isAuth, updatePassword);
-router.put("/:id", isAuth, uploadFile, updateProfile);
+router.post("/update-password/:id", isAuth, updatePassword);
+router.put("/update-profile/:id", isAuth, uploadFile, updateProfile);
 router.post("/follow/:id", isAuth, followandUnfollowUser);
 router.get("/followdata/:id", isAuth, userFollowerandFollowingData);
 
